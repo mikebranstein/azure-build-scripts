f365478eefd136d1d31a9d300c13b06acd2a6187
Mike Branstein <mike.branstein@gmail.com>
2019-08-21 20:41:30 -0400

    add msi

--- terraform-workshop/main.tf
+++ terraform-workshop/main.tf
@@ -6,6 +6,7 @@ variable "subscription_id" { }
 provider "azurerm" {
     tenant_id       = "${var.tenant_id}"
     subscription_id = "${var.subscription_id}" 
+    use_msi         = true
 }
 
 resource "azurerm_resource_group" "rg" {
