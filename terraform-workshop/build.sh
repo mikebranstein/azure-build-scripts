b9392c2006ff8874928fa90674f6b41385feb810
Mike Branstein <mike.branstein@gmail.com>
2019-08-21 20:33:30 -0400

    updates

--- terraform-workshop/build.sh
+++ terraform-workshop/build.sh
@@ -1,4 +1,10 @@
 
+# execute this script
+# curl -LOk https://github.com/mikebranstein/azure-build-scripts/archive/master.zip | unzip;
+
+export TF_VAR_tenant_id=$(az account list --query "[0].tenantId")
+export TF_VAR_subscription_id=$(az account list --query "[0].id")
+
 # run terraform
 terraform init
 terraform apply
