dd6ea4ba02913751db46a8b4a25b1d2871f9db1d
Mike Branstein <mike.branstein@gmail.com>
2019-08-21 20:43:20 -0400

    remove double-quotes

--- terraform-workshop/build.sh
+++ terraform-workshop/build.sh
@@ -2,8 +2,8 @@
 # execute this script
 # [ -d "azure-build-scripts-master" ] && rm -rf azure-build-scripts-master;curl -LOk https://github.com/mikebranstein/azure-build-scripts/archive/master.zip; unzip master.zip; cd azure-build-scripts-master/terraform-workshop; chmod +x build.sh; ./build.sh
 
-export TF_VAR_tenant_id=$(az account list --query "[0].tenantId")
-export TF_VAR_subscription_id=$(az account list --query "[0].id")
+export TF_VAR_tenant_id=$(az account list --query "[0].tenantId" -otsv)
+export TF_VAR_subscription_id=$(az account list --query "[0].id" -otsv)
 
 # run terraform
 terraform init
