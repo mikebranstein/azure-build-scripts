7b0154e923af0106124d4e835d839176d8800138
Mike Branstein <mike.branstein@gmail.com>
2019-08-21 20:53:27 -0400

    add storage account

--- terraform-workshop/main.tf
+++ terraform-workshop/main.tf
@@ -13,3 +13,15 @@ resource "azurerm_resource_group" "rg" {
     name = "tf-az-workshop-rg"
     location = "East US"
 }
+
+locals {
+    random_hash = substr(md5(azurerm_resource_group.rg.id), 0, 5)
+}
+
+resource "azurerm_storage_account" "testsa" {
+  name                     = "workshopsa${local.random_hash}"
+  resource_group_name      = "${azurerm_resource_group.testrg.name}"
+  location                 = "East US"
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
