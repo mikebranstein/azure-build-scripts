5cbcb818aee50fbe2f117ca63b185886861e1fb6
Mike Branstein <mike.branstein@gmail.com>
2019-08-21 21:16:49 -0400

    add vm

--- terraform-workshop/main.tf
+++ terraform-workshop/main.tf
@@ -1,8 +1,6 @@
 variable "tenant_id" { }
 variable "subscription_id" { }
 
-
-
 provider "azurerm" {
     tenant_id       = "${var.tenant_id}"
     subscription_id = "${var.subscription_id}" 
@@ -18,10 +16,78 @@ locals {
     random_hash = substr(md5(azurerm_resource_group.rg.id), 0, 5)
 }
 
-resource "azurerm_storage_account" "testsa" {
-  name                     = "workshopsa${local.random_hash}"
+resource "azurerm_storage_account" "sa" {
+  name                     = "tfazworkshopsa${local.random_hash}"
   resource_group_name      = "${azurerm_resource_group.rg.name}"
-  location                 = "East US"
+  location                 = "${azurerm_resource_group.rg.location}"
   account_tier             = "Standard"
   account_replication_type = "LRS"
+}
+
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = "tf-az-workshop-nvet"
+  address_space       = ["10.0.0.0/16"]
+  location            = "${azurerm_resource_group.rg.location}"
+  resource_group_name = "${azurerm_resource_group.rg.name}"
+}
+
+resource "azurerm_subnet" "subnet" {
+  name                 = "default"
+  resource_group_name  = "${azurerm_resource_group.rg.name}"
+  virtual_network_name = "${azurerm_virtual_network.vnet.name}"
+  address_prefix       = "10.0.0.0/24"
+}
+
+resource "azurerm_network_interface" "nic" {
+  name                = "tf-az-workshop-vm-nic"
+  location            = "${azurerm_resource_group.rg.location}"
+  resource_group_name = "${azurerm_resource_group.rg.name}"
+
+  ip_configuration {
+    name                          = "tf-az-workshop-vm-nic-priv-config"
+    subnet_id                     = "${azurerm_subnet.subnet.id}"
+    private_ip_address_allocation = "Dynamic"
+  }
+
+  ip_configuration {
+    name                          = "tf-az-workshop-vm-nic-pub-config"
+    public_ip_address_id          = "${azurerm_public_ip.pip.id}"
+  }
+}
+
+resource "azurerm_public_ip" "pip" {
+  name                = "tf-az-workshop-vm-pip"
+  location            = "${azurerm_resource_group.rg.location}"
+  resource_group_name = "${azurerm_resource_group.rg.name}"
+  allocation_method   = "Dynamic"
+}
+
+resource "azurerm_virtual_machine" "vm" {
+  name                  = "tf-az-workshop-vm"
+  location              = "${azurerm_resource_group.rg.location}"
+  resource_group_name   = "${azurerm_resource_group.rg.name}"
+  network_interface_ids = ["${azurerm_network_interface.nic.id}"]
+  vm_size               = "Standard_DS4_v3"
+
+  delete_os_disk_on_termination = true
+  delete_data_disks_on_termination = true
+
+  storage_image_reference {
+    publisher = "MicrosoftWindowsServer"
+    offer     = "WindowsServer"
+    sku       = "2019-Datacenter"
+    version   = "latest"
+  }
+  storage_os_disk {
+    name              = "tfazworkshopvmdisk01"
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+    managed_disk_type = "Standard_LRS"
+  }
+  os_profile {
+    computer_name  = "workshop-vm"
+    admin_username = "workshopadmin"
+    admin_password = "P.$$w0rd1234"
+  }
 }
