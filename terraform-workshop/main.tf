657e374dd1986de9a2fe2990340a34739a36116d
Mike Branstein <mike.branstein@gmail.com>
2019-08-21 20:54:05 -0400

    fix rg ref

--- terraform-workshop/main.tf
+++ terraform-workshop/main.tf
@@ -20,7 +20,7 @@ locals {
 
 resource "azurerm_storage_account" "testsa" {
   name                     = "workshopsa${local.random_hash}"
-  resource_group_name      = "${azurerm_resource_group.testrg.name}"
+  resource_group_name      = "${azurerm_resource_group.rg.name}"
   location                 = "East US"
   account_tier             = "Standard"
   account_replication_type = "LRS"
