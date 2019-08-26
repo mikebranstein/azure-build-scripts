0bc53a4153641c070e34dec12cacda2e6a42a6b5
Mike Branstein <mike.branstein@gmail.com>
2019-08-26 16:58:53 -0400

    revert

--- terraform-workshop/build.sh
+++ terraform-workshop/build.sh
@@ -3,17 +3,8 @@
 # 1. Ask attendees to open a Cloud Shell (bash)
 # 2. Have them paste the following command into the Cloud Shell, then wait for it to complete
 # 
-# bash <(curl -LOks https://raw.githubusercontent.com/mikebranstein/azure-build-scripts/master/terraform-workshop/build.sh)
+# [ -d "azure-build-scripts-master" ] && rm -rf azure-build-scripts-master;curl -LOk https://github.com/mikebranstein/azure-build-scripts/archive/master.zip; unzip master.zip; cd azure-build-scripts-master/terraform-workshop; chmod +x build.sh; ./build.sh
 
-# clean up environment before running
-[ -d "azure-build-scripts-master" ] && rm -rf azure-build-scripts-master
-
-# grab all scripts and unzip
-curl -LOk https://github.com/mikebranstein/azure-build-scripts/archive/master.zip
-unzip master.zip
-cd azure-build-scripts-master/terraform-workshop
-
-# get tenant and subscription
 export TF_VAR_tenant_id=$(az account list --query "[0].tenantId" -otsv) 
 export TF_VAR_subscription_id=$(az account list --query "[0].id" -otsv) 
 
