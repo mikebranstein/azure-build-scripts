944cfd593eedfcfb4c4cc76e5c6177bd44fc6d42
Mike Branstein <mike.branstein@gmail.com>
2019-09-22 18:23:48 -0400

    add app setting to TF

--- api-management-demo/main.tf
+++ api-management-demo/main.tf
@@ -40,6 +40,10 @@ resource "azurerm_app_service" "app" {
     location            = "East US"
     resource_group_name = "${azurerm_resource_group.rg.name}"
     app_service_plan_id = "${azurerm_app_service_plan.standard_app_plan.id}"
+
+    app_settings {
+        "SCM_DO_BUILD_DURING_DEPLOYMENT" = "true"
+    }
 }
 
 resource "null_resource" "enable_compilation" {
