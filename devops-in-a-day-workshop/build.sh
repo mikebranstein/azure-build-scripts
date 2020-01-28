3b176a5df188269e6d3176c7a7bdde8ec0228938
Mike Branstein <mike.branstein@gmail.com>
2020-01-28 15:21:34 -0500

    fix typo

--- devops-in-a-day-workshop/build.sh
+++ devops-in-a-day-workshop/build.sh
@@ -7,7 +7,7 @@
 
 export TF_VAR_tenant_id=$(az account list --query "[0].tenantId" -otsv) 
 export TF_VAR_subscription_id=$(az account list --query "[0].id" -otsv) 
-export TF_VAR_resource_group_name=$(az group list --query "[0].id" -otsv)
+export TF_VAR_resource_group_name=$(az group list --query "[0].name" -otsv)
 
 # run terraform
 terraform init
