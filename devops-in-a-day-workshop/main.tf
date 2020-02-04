9e9ad8798fb49f1bac38a3031880a08f5213213a
Mike Branstein <mike.branstein@gmail.com>
2020-02-04 08:46:20 -0500

    update vm size

--- devops-in-a-day-workshop/main.tf
+++ devops-in-a-day-workshop/main.tf
@@ -58,7 +58,7 @@ resource "azurerm_virtual_machine" "vm" {
   location              = "${var.location}"
   resource_group_name   = "${var.resource_group_name}"
   network_interface_ids = ["${azurerm_network_interface.nic.id}"]
-  vm_size               = "Standard_DS4_v2"
+  vm_size               = "Standard_DS2_v2"
 
   delete_os_disk_on_termination = true
   delete_data_disks_on_termination = true
